d64c30e7ac14349dc17f8f595d21b250ee422191
Nisha <sidratulmuntahanisha83@gmail.com>
2025-12-14 14:03:51 +0600

    first commit with first layout

---
+++ articles.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bangladesh Travel Diaries</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="main-container">
+<h2 class="title">Welcome to ‘Bangladesh Travel Diaries’!</h2>
+
+<form action="#" method="post" autocomplete="off" class="username-form" onsubmit="return false;">
+<label for="name">Enter your username:</label>
+<input type="text" id="name" placeholder="Type your username here" autocomplete="off"/>
+<button type="submit" id="signin-btn">Sign in</button>
+</form>
+</div>
+</body>
+</html>
